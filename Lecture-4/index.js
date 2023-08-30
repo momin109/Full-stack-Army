ffcce9b62f091158d3b8bc94609f98d1a34983a5
momin109 <momin550550@gmail.com>
2023-08-30 21:38:59 +0600

    first commit

---
+++ Lecture-4/index.js
@@ -0,0 +1,175 @@
+/*
+console.log('abdul momin', 'abdul momin'.length);
+throw new error('something is rong')
+*/
+
+///////////////////////////////////////////////
+/* const names = ['abu raihan', 'abdul momin', 'kutubul alam', 'bablu rahman', 'hamid', 'hanif', 'faruk', 'tutul']
+
+let index = 0;
+let name = names[index];
+console.log(name)
+
+setInterval(() => {
+    name = names[index++];
+    console.log(name, name.length)
+
+    if (index === names.length) {
+        index = 0
+    }
+}, 1000)
+ */
+
+
+// ////////////////
+// Scenario 1- single brunch
+// if condition
+/*
+if (hasMoney) {
+    purchessPhone()
+}
+
+// Scenario 2- two brunch
+// if else condition
+
+if (toss === head) {
+    win()
+} else {
+    loss()
+}
+
+// scenario 3 - mulple brunch
+if (1 > 1) {
+    big()
+} else if (1 < 1) {
+
+}
+ */
+
+
+/* // zokhon amra zanbo na tokhon koto khon pozonto cholbe tokhon (while loop) use korbo
+// while (when we don't the range)
+while (true) {
+    let num = Math.ceil(Math.random() * 100)
+    console.log('abdul momin', num)
+    if (num === 11) break;
+}
+
+ */
+
+
+/* // do while condition false takle ak bar chalbe,
+do {
+    console.log('what is your name')
+} while (false)
+ */
+
+// array
+
+/*
+// const students = ['Momin', 'Ruhul', 'Samim', 'Kudus', 'Billal']
+
+for (let i = 0; i < students.length; i++) {
+    console.log(students[i], students[i].toLowerCase())
+
+}
+
+////
+const nums = [1, 2, 3, 4, 5]
+const bools = [true, false, true, false]
+const nulls = [null, null, null];
+const us = [undefined, undefined, undefined];
+const arrayOfArray = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+const mixed = [null, undefined, true, false, 'ssh', 333]
+
+const object = [
+    names = {
+        name: 'momin',
+        id: '33',
+        address: 'charmohonpur'
+    }
+]
+
+const functionArray = [
+    function sum() {
+        console.log(3 + 4)
+    }
+]
+ */
+
+// object
+/////////////////////////////////////////
+/* 
+const student1 = {
+    firstName: 'abu',
+    lastName: ' raihan',
+    email: 'aburaihan@gmail.com',
+}
+
+const student2 = {
+    firstName: 'kamal',
+    lastName: ' uddin',
+    email: 'kamaluddin@gmail.com'
+}
+const student3 = {
+    firstName: 'faruk',
+    lastName: ' Ahmed',
+    email: 'farukAhmed@gmail.com'
+}
+
+const allStudent = [student1, student2, student3]
+for (let i = 0; i < allStudent.length; i++) {
+    sendEmail(allStudent[i].email)
+}
+
+// sendEmail(student1.email)
+
+function sendEmail(email) {
+    console.log('sending to eamil :', email)
+}
+
+allStudent.forEach(item => console.log(item.email))
+allStudent.forEach(item => console.log('fulName: ', item.firstName, item.lastName))
+
+ */
+/* 
+function nameOfFunction(name) {
+    if (!name) {
+        console.log('please provide your name')
+    } else {
+        console.log('Welcome', name)
+    }
+
+}
+
+nameOfFunction('abdul samad')
+nameOfFunction()
+nameOfFunction()
+ */
+
+function generalRandomNumber(min, max) {
+    const randomeNumber = Math.round(Math.random() * max)
+    return randomeNumber;
+}
+
+console.log(generalRandomNumber(5, 10))
+console.log(generalRandomNumber(50, 100))
+console.log(generalRandomNumber(500, 1000))
+
+
+/// express holo kono kichu akta return kore
+// exmple:
+// console.log('this is express')
+// name('this express')
+// const name = ()=> { this is express}
+
+// statement kono kichu return kore na
+// exmple: for loop, object , array
+// const name = 'abdul momin'
+//function name(){ 'this is statment'}
+//return name; aita statment
---
+++ Lecture-5/app.js
@@ -0,0 +1,68 @@
+const numbers = [1, 2, 4, 7, 9, 13, 45, 37];
+// // sob element start teke end pozonto zao ai ta ke travers bolle
+// let sum = 0;
+// for (let i = 0; i < numbers.length; i++) {
+//     sum += numbers[i];
+
+// }
+// console.log(sum)
+
+// //no. 1
+// numbers.forEach(() => { // ai ta arrow function
+
+// })
+// //no. 2
+// numbers.forEach(function () {  //normal function
+
+// })
+// //no. 3
+// const cb = () => {
+//     console.log('hello')
+// }
+// numbers.forEach(item => cb(console.log(item)))
+// // no.4
+// const cb2 = () => {
+//     console.log('hello')
+// }
+// numbers.forEach(cb2)
+
+// no.5
+
+// numbers.forEach(() => { // ai khane amra bai teke call kori ni. // kintu forEach call koreche
+//     console.log()
+// })
+
+
+numbers.forEach(function (value, index, arr) {  //1. protom kono kichu parameter dele seta (value return) korbe 2. second paramiter dele (index number asbe) 3.// three parameter dele seta (arr asbe) [numbers.forEach(function (value, index, arr) { ]
+    // console.log(value, index, arr)
+})
+
+numbers.forEach(function (_, index) { // zodi value dorkar na hole (_) dete hobe (_) ar mane holo value ke buzano hoiche
+    // console.log(index)
+})
+
+numbers.forEach(function (_, __, arr) { // zodi array dorkar holo (value,index,arr) ai vabe lekhte pari or aivabe lekha zai (_, __, arr)
+    // console.log(arr)
+})
+
+// for loop kono kichu return kore na forEach kono kichu return kore na
+numbers.forEach(function (value) { // zor songkha ber kora
+    if (value % 2 === 0) {
+        // console.log(value)
+    }
+})
+//#
+// let sum = 0;
+// numbers.forEach(function (v) {
+//     sum += v
+// })
+// console.log(sum)
+
+////////////////////////
+let sum = 0;
+numbers.forEach(function (v, i) {
+    if (i <= 3) {  // value 0 teke 3 pozonto zoto gulo sonkha sob gulo zog korbe
+        sum += v
+    }
+})
+console.log(sum)
